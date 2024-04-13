b9130f8b24203cebe174bda3757a2ccadc05caed
fersh00 <fc323897@gmail.com>
2024-04-12 21:52:47 -0700

    Se crea Couter.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { useState,useEffect } from 'react'
 import 'bulma/css/bulma.css'
+import Counter from './components/Counter'
+//import axios from 'axios'
 
 function App() {
-	const [count, setCount] = useState(0)
-  useEffect(() => {
-    console.log('cargo')      
-  }, [count])
-  
-
 	return (
-    <>
-      <h1 className='is-size-1' >Counter</h1>
-      <button className='button is-large is-white'  onClick={() => setCount(count + 1)}>+</button>
-      <p className='is-size-2'>{count}</p>
-    </>
-  )
+		<>
+			<h1 className=' title is-size-1 has-text-centered'>App</h1>
+
+			<Counter />
+		</>
+	)
 }
 
 export default App
